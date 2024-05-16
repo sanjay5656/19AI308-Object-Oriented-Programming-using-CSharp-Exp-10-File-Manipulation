5c8e7ff98224b08b2819952d4ce88ec425d6ca45
SANJAY S <115128955+sanjay5656@users.noreply.github.com>
2024-05-16 17:42:10 +0530

    san

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace DemoApplication
+struct UserData
 {
-    struct student
+    public string Name;
+    public int Age;
+    public string Email;
+}
+class Program
+{
+    static void Main(string[] args)
     {
-           // Write Your Code Here
-    };
-    class Tutorial
-    {
-
-        static void Main(string[] args)
+        string filePath = @"C:\Users\SEC\Desktop\exp10.txt";
+        UserData[] ud=new UserData[3];
+        for(int i=0; i<3;i++)
         {
-            // Write Your Code Here
+            Console.WriteLine("Enter your name:");
+            ud[i].Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            ud[i].Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your email:");
+            ud[i].Email = Console.ReadLine();
+
+            WriteUserDataToFile(filePath, ud[i]);
+        }  
+        Console.WriteLine("User data has been saved to the file.");
+    }
+
+    static void WriteUserDataToFile(string filePath, UserData userData)
+    {
+        //using (FileStream fs = new FileStream(filePath, FileMode.Create))
+        using (StreamWriter writer = File.AppendText(filePath))
+        {
+            writer.WriteLine($"Name: {userData.Name}");
+            writer.WriteLine($"Age: {userData.Age}");
+            writer.WriteLine($"Email: {userData.Email}");
         }
     }
 }
